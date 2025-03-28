50bbc4abbce128a5c1d70b2909169cb2b5e17966
dear  Halimeh <nahalimeh@gmail.com>
2025-03-28 17:07:04 +0430

    create folgers in Components

---
+++ pro2/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+// import { BrowserRouter, Route, NavLink } from "react-router-dom";
+import Reservation from "./Components/Reservation";
+
+function App() {
+  //main page
+  return (
+    <div className="App">
+      <Reservation />
+    </div>
+  );
+}
+
+export default App;
